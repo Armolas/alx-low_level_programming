f906d733af20e08b7c15f6159dacbed126cfd85d
Armolas <armolas06@gmail.com>
2022-09-09 11:14:25 -0700

    created 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int fig = 0;
+
+	while (fig <= 9)
+	{
+	printf("%d", fig);
+	fig++;
+	}
+	putchar('\n');
+	return (0);
+}
