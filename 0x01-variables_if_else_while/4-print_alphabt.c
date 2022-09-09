d85415087b9dba539a0f5fcfbf9ed924d9c2dc06
Armolas <armolas06@gmail.com>
2022-09-09 10:55:55 -0700

    created 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low = 'a';
+
+	for (low;low <= 'z';low++)
+	{
+	if (low == 'e' || low == 'q')
+	{
+	continue;
+	}
+	putchar(low);
+	}
+	putchar('\n');
+	return (0);
+}
