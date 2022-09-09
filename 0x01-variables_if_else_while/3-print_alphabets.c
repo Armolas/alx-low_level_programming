0dfb86e2e287026f44d874d797d4a9fc67f668e9
Armolas <armolas06@gmail.com>
2022-09-09 09:39:51 -0700

    created 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low = 'a';
+	char cap = 'A';
+
+	while (low <= 'z')
+	{
+	putchar(low);
+	low++;
+	}
+	while (cap <= 'Z')
+	{
+	putchar(cap);
+	cap++;
+	}
+	putchar('\n');
+	return (0);
+}
