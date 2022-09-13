a91be80442c8f07cfb68223ddeb9cd7c4d245356
Armolas <armolas06@gmail.com>
2022-09-13 12:10:21 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - retuns the absolute value of an integer
+ * @n: the number to check
+ *
+ * Return: absolute integer
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+	return (n);
+	}
+	else
+	{
+	return (n * -1);
+	}
+}
