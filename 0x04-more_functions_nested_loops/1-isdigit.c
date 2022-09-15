9d884e9bdac8857c6fbf5d9dc7c9545dee9f1910
Armolas <armolas06@gmail.com>
2022-09-15 08:10:56 -0700

    created a digit checker

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digits between 0 and 9
+ * @c: the arguement of the function
+ *
+ * Return: 1 if c is a digit 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
