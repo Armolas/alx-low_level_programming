6b3bf7166449f3c06b46fefe4fb339d06a9db964
Armolas <armolas06@gmail.com>
2022-09-15 08:30:30 -0700

    created a function that prints 0 through 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function to print 0 through 9
+ *
+ *Return: void
+ */
+void print_numbers(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+	_putchar(num);
+	num++;
+	}
+	_putchar('\n');
+}
