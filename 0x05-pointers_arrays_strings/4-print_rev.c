587254ec7bc45e60629527b9c40782a35e72c6a0
Armolas <armolas06@gmail.com>
2022-09-19 14:28:28 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: the string
+ */
+void print_rev(char *s)
+{
+	int a, b;
+
+	for (a = 0 ; s[a] != '\0' ; a++)
+	{}
+	for (b = a ; b >= 0 ; b--)
+	{
+		_putchar(s[b]);
+	}
+}
