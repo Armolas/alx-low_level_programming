bd1eb18e55cc867e37557a7f22b7af8747254da4
Armolas <armolas06@gmail.com>
2022-09-19 08:11:54 -0700

    created 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the value of the parameter to 98
+ * @n: the parameter of the function
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
