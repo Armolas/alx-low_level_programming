f2549e41383baa11b647e576249ef11f3aeb99ee
Armolas <armolas06@gmail.com>
2022-09-19 15:15:07 -0700

    arcade

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts_half - prints the last half of a string
+ * @str: the string
+ */
+void puts_half(char *str)
+{
+	int i, j;
+
+	if ((_strlen(str) % 2) != 0)
+	{
+		j++;
+	}
+	for (i = ((_strlen(str) + j) / 2) ; i < _strlen(str) ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
