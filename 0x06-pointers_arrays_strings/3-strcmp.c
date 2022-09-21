3732f3878c617c3e06facdf0823095fe62789e9a
Armolas <armolas06@gmail.com>
2022-09-21 10:13:58 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two string
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: returns 0 if the strings are the same, positive if s1 is greater than s2 and negative if s2 is greater than s1
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, value = 0;
+
+	for (i = 0 ; s1[i] != '\0' || s2[i] != '\0' ; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			if (s1[i] > s2[i])
+			{
+				value = 1;
+			}
+			else
+			{
+				value = -1;
+			}
+		}
+		break;
+	}
+	return (value);
+}
