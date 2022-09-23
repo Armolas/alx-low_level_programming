d503302030d80d27642783f895922a654a4be489
Armolas <armolas06@gmail.com>
2022-09-22 17:20:18 -0700

    Whew!

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,5 +11,9 @@ char *leet(char *);
 char *cap_string(char *);
 char *string_toupper(char *);
 void print_number(int n);
+#include <stdio.h>
+void print_line(char *c, int s, int l);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
