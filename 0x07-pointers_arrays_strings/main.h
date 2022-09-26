e6daa3eac35428a2b79343645cb2bd6c8de42124
Armolas <armolas06@gmail.com>
2022-09-26 15:17:24 -0700

    chess

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ char *_strchr(char *s, char c);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
 
 #endif /*MAIN_H*/
