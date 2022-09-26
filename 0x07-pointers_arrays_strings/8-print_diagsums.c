55aafa50cef3d0dcf96179d953741ae6c4677ba3
Armolas <armolas06@gmail.com>
2022-09-26 16:49:14 -0700

    correction

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,20 +7,15 @@
  *@a: matrix
  *@size: matrix size
  */
-void print_diagsums(int (*a)[size][size], int size)
+void print_diagsums(int *a, int size)
 {
-	int i, j;
+	int i;
 	int sumA = 0;
 	int sumB = 0;
 
 	for (i = 0 ; i < size ; i++)
 	{
-		for (j = size - (i + 1) ; j >= 0 ; j--)
-		{
-			sumA = sumA + a[i][j];
-			sumB = sumB + a[i][i];
-			break;
-		}
-	}
-	printf("%d, %d", sumB, sumA);
+		sumB += a[(i * size) + i];
+		sumA += a[(size - 1) + ((size - 1) * i)];
+	printf("%d, %d\n", sumB, sumA);
 }
