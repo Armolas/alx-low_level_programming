58a773cfa1db4f7153537177dbd988c82ea2469e
Armolas <armolas06@gmail.com>
2022-09-28 10:13:21 -0700

    These tasks are HARD!!!

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -26,5 +26,5 @@ int _sqrt_recursion(int n)
 {
 	if (n == 0)
 		return (0);
-	return (guess(1, n)):
+	return (guess(1, n));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -28,5 +28,5 @@ int is_prime_number(int n)
 {
 	if (n == 2)
 		return (1);
-	return (pcheck(2, n)):
+	return (pcheck(2, n));
 }
