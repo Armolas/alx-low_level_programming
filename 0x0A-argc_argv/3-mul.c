6fdb63bfee815d35a8831833b1aad1bae9e65166
Armolas <armolas06@gmail.com>
2022-09-30 04:55:24 -0700

    readme

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,7 +13,10 @@ int main(int argc, char *argv[])
 	int mul;
 
 	if (argc < 3)
+	{
 		printf("Error\n");
+		return (1);
+	}
 	mul = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", mul);
 	return (0);
