9f42e4a0ea20ec16c7e1bcb0004cccb4a96ee2ea
Armolas <armolas06@gmail.com>
2022-09-30 08:41:44 -0700

    static

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns lenght of a string
+ * @s: the string to return
+ *
+ * Return: the lenght of a string
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0 ; s[a] != '\0' ; a++)
+	{
+	}
+	return (a);
+}
