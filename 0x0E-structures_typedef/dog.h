77b5815e274264655c71984a2ca58ad3d159a331
Armolas <armolas06@gmail.com>
2022-10-10 16:31:02 -0700

    added headers

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
  * struct dog - structure for dog info
@@ -13,4 +13,10 @@ struct dog
 	float age;
 	char *owner;
 };
+
+int _putchar(char);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
