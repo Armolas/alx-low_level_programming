4cd01307fd5b2e3e965aae00e940d6123fdc6920
Armolas <armolas06@gmail.com>
2022-10-13 17:15:04 -0700

    iso

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,13 +9,12 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
-		return (0);
-
 	unsigned int i;
-	unsigned int sum = 0;
+	int sum = 0;
 	va_list ap;
 
+	if (n == 0)
+		return (0);
 	va_start(ap, n);
 	for (i = 0 ; i < n ; i++)
 	{
