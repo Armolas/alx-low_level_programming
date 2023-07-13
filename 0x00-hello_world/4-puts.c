7f22962f1d5f5d33aaa068e9096264708b84c037
armolas <armolas06@gmail.com>
2023-07-13 16:12:07 -0700

    hello_world project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char my_str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(my_str);
+	return (0);
+}
