c5e096338374e693ff2aaff05404b1c1a7401784
armolas <armolas06@gmail.com>
2023-07-13 17:56:13 -0700

    hello_world project

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t bytes_written = write(STDERR_FILENO, message, strlen(message));
+	char *mess = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t bytes_written = write(STDERR_FILENO, mess, strlen(mess));
 
 	if (bytes_written == -1)
 	{
