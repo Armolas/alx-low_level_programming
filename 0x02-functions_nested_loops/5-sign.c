2ef64dccdad0ebe714bf149a73234946a8e84b32
armolas <armolas06@gmail.com>
2023-07-19 04:13:12 -0700

    nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: number whose sign is to be printed
- * Return : 1 if n is positive, 0 if n is zero and -1 if n is negative
+ * Return: 1 if n is positive, 0 if n is zero and -1 if n is negative
  */
 int print_sign(int n)
 {
@@ -14,13 +14,13 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-		_putchar(n);
-		return(0);
+		_putchar('0');
+		return (0);
 	}
 
 	else
 	{
 		_putchar('-');
-		return(-1);
+		return (-1);
 	}
 }
