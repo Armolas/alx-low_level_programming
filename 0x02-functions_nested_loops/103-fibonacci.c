c76359fc56bffd14134797954e257aebbca5ec39
armolas <armolas06@gmail.com>
2023-07-19 20:03:21 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	unsigned long a = 0;
+	unsigned long b = 1;
+	unsigned long sum;
+	unsigned long sum_1 = 0;
+
+	while (1)
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+		if (sum < 4000000 && sum % 2 == 0)
+		{
+			sum_1 += sum;
+		}
+		else if (sum < 4000000)
+			continue;
+		else
+			break;
+	}
+	printf("%lu\n", sum_1);
+	return (0);
+}
