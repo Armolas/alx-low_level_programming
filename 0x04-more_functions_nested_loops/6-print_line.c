9b02ee65bc3fccb2a77cfa7e94a521474cf84f52
armolas <armolas06@gmail.com>
2023-07-20 16:40:21 -0700

    more functions nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - prints a line
+ * @n: length of line
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		return;
+	for (i = 0 ; i < n ; i++)
+		_putchar('_');
+	_putchar('\n');
+}
