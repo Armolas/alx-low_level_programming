52a4638a6b3f06a1d7e84fa0de83d0d82cc09808
armolas <armolas06@gmail.com>
2023-07-19 20:42:38 -0700

    nested loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,9 +6,9 @@
 int main(void)
 {
 	int i;
-	unsigned long a = 0;
-	unsigned long b = 1;
-	unsigned long sum;
+	unsigned long int a = 0;
+	unsigned long int b = 1;
+	unsigned long int sum;
 
 	for (i = 0 ; (i < 98) ; i++)
 	{
