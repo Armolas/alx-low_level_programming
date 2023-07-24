00d66c9d6434045bb5ff9bb85db76ff9be674e10
armolas <armolas06@gmail.com>
2023-07-24 15:01:43 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: the strring
+ */
+void print_rev(char *s)
+{
+	int i, j = 0 ;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		j++;
+	for (i = j ; i >= 0 ; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
