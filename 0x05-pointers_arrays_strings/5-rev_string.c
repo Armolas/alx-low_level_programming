719ed085c284c63faed5cad6d10c8ec8fe3ca35d
armolas <armolas06@gmail.com>
2023-07-24 16:44:29 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/***
+ * rev_string - reverses a string
+ * @s: the string
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char c;
+
+	for (i = 0 ; s[i] != '\0' ; ++i)
+		;
+	for (j = 0 ; j < i / 2 ; ++j)
+	{
+		c = s[j];
+		s[j] = s[i-1-j];
+		s[i-1-j] = c;
+	}
+}
