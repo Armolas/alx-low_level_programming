dd05c9c8f5a642843e3bf80bfbe9f0adf1a65aa0
armolas <armolas06@gmail.com>
2023-07-24 17:09:40 -0700

    pointers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,9 +9,19 @@ void puts_half(char *str)
 
 	for (j = 0 ; str[j] != '\0' ; j++)
 		;
-	for (i = j / 2 ; str[i] != '\0' ; i++)
+	if (j % 2 != 0)
 	{
-		_putchar(str[i]);
+		for (i = (j - 1) / 2 ; str[i] != '\0' ; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = j / 2 ; str[i] != '\0' ; i++)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
