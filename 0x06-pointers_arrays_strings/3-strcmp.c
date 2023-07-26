2eede98f6fc1bf028f8093dd857d77776d6e914e
armolas <armolas06@gmail.com>
2023-07-26 15:19:48 -0700

    more pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,14 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0 ; s1[i] != '\0' ; i++)
+	for (i = 0 ; s1[i] != '\0' && s2[i] != '\0' ; i++)
 	{
 		if (s1[i] == s2[i])
 			continue;
 		else if (s1[i] > s2[i])
-			return (15);
+			return (s1[i] - s2[i]);
 		else
-			return (-15);
+			return (s2[i] - s1[i]);
 	}
 	return (0);
 }
