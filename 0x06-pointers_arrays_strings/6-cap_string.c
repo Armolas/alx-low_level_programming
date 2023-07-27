8d70959a2b1f113cc91ef2e540397c7b758680f5
armolas <armolas06@gmail.com>
2023-07-26 17:24:38 -0700

    more pointers

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes every word of a string
+ * @s: the string
+ * Return: pointer to s
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] = s[0] - 32;
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		if ((s[i] >= 'a' && s[i] <= 'z') && (s[i - 1] == (' ' || '\t' || '\n' || ',' || ';' || '.' || '!' || '?' || '"' || '(' || ')' || '{' || '}')))
+			s[i] = s[i] - 32;
+	}
+	return (s);
+}
