bb5d17dc532cc9b507a2d3169186e995450a523c
armolas <armolas06@gmail.com>
2023-08-01 16:37:35 -0700

    ointers

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,7 +11,11 @@ char *_strchr(char *s, char c)
 	int i;
 
 	if (c == '\0')
-		return (NULL);
+	{
+		for (i = 0 ; s[i] != '\0' ; i++)
+			;
+		return (s + i);
+	}
 	for (i = 0 ; s[i] != '\0' ; i++)
 	{
 		if (s[i] == c)
