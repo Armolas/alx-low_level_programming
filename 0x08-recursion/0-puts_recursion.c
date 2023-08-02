7bf64c78bdba6a7230b923ad69d294c83b1f7661
armolas <armolas06@gmail.com>
2023-08-02 08:16:02 -0700

    recursions

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string
+ * @s: the string to print
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
