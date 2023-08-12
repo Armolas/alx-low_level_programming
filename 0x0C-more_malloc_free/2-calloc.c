4578548e4710c2f770c2a5446788da48d5cba8a7
armolas <armolas06@gmail.com>
2023-08-12 09:01:20 -0700

    more malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ptr;
 
 	if (nmemb == 0 || size == 0)
-		exit(0);
+		return (NULL);
 	mem = malloc(nmemb * size);
 	ptr = mem;
 	if (!mem)
