b9016dc988322dfcb4f1a40ea7c803b178482a3d
armolas <armolas06@gmail.com>
2023-08-14 09:35:07 -0700

    macros

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - creates a new dog rofile
+ * @name: dog name
+ * @owner: owner name
+ * @age: dog age
+ */
 struct dog{
 	char *name;
 	float age;
