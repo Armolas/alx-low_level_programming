756ada0e83580fd257e291e08c6642f104750578
armolas <armolas06@gmail.com>
2023-08-14 10:10:34 -0700

    macros

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,19 +1,14 @@
-#include "main.h"
+#include "dog.h"
 /**
  * init_dog - initializes a dog profile
  * @d: dog struct
  * @name: dog name
  * @age: dog age
  * @owner: owner name
- * Return: values
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
-	if (!d)
-		return (NULL);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	return (d);
 }
