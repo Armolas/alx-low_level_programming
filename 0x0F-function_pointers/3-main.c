741a2ac90f16bf6fe651575ab6a243b4ed139c57
armolas <armolas06@gmail.com>
2023-08-16 18:28:13 -0700

    function pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a = atoi(argv[1]);
+	int b = atoi(argv[3]);
+	char *op = argv[2];
+	int res;
+	int (*f)(int a, int b);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	f = (get_op_func(op));
+	if (f == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+	res = f(a, b);
+	printf("%d\n", res);
+	return (0);
+}
