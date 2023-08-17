1290464b47f05d8fe2a2a40b59a49ced65fc3ba2
armolas <armolas06@gmail.com>
2023-08-17 15:54:54 -0700

    variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,8 +10,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list arg_list;
 	char *sp;
 
-	if (n == 0)
-		return;
 	va_start(arg_list, n);
 	for (i = 0 ; i < n ; i++)
 	{
