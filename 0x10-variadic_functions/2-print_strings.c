9fbdf418d79b738886600a0c9e5671280a7dee9e
armolas <armolas06@gmail.com>
2023-08-19 09:12:00 -0700

    print

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (sp)
 			printf("%s", sp);
 		else
-			printf("nil");
+			printf("(nil)");
 		if (separator && i != n - 1)
 			printf("%s", separator);
 	}
