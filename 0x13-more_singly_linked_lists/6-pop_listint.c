c6273207b753198373ed840d780cc344572f3766
armolas <armolas06@gmail.com>
2023-08-28 15:50:51 -0700

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * pop_listint - deletes the head node
+ * @head: head of list
+ * Return: head node's integer
+ */
+int pop_listint(listint_t **head)
+{
+	int i;
+
+	if (!*head)
+		return (0);
+	i = head->n;
+	head = head->next;
+	return (i);
+}
