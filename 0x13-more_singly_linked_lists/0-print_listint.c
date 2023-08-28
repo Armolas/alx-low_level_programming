1fa5189a172ef5c542d4b7f99bf5e7e1990b33b0
armolas <armolas06@gmail.com>
2023-08-28 13:53:43 -0700

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_listint - prints all elements in a list
+ * @h: head of the list
+ * Return: number of elements in list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	if (!h)
+		return (0);
+	printf("%d", h->n);
+	i += print_listint(listint_t h->next);
+	return (i + 1);
+}
