fd9319a97e17094642633e5f134a128356fd3633
armolas <armolas06@gmail.com>
2023-08-28 16:22:06 -0700

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_listint - sums all integers of a list
+ * @head: head of list
+ * Return: sum of all integers
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (!head)
+		return (0);
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
