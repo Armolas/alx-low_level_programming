74ca047aa6641755c10c00b2074e67d41d2d4df2
armolas <armolas06@gmail.com>
2023-08-28 16:12:46 -0700

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - returns the node at index
+ * @head: head of list
+ * @index: nth node to find
+ * Return: pointer to nth node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (!head)
+		return (NULL);
+	while (i < index)
+	{
+		head = head->next;
+		i++;
+	}
+	if (!head)
+		return (NULL);
+	return (head);
+}
