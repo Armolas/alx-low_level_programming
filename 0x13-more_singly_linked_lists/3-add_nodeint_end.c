b9d16feecc48c35f02e4bfe4f79f66c4a6635b4b
armolas <armolas06@gmail.com>
2023-08-28 14:29:31 -0700

    more singly linked lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,11 +1,11 @@
 #Include "lists.h"
 /**
- * add_nodeint - adds a new element to the list
+ * add_nodeint_end - adds a new element to the list
  * @head: head of the list
  * @n: integer to be stored in the list
  * Return: pointer to new element
  */
-listint_t *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t new, *currentList;
 
