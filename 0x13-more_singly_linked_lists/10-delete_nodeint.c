e6ca8dd89717914e3799b44475088813ff5463a9
armolas <armolas06@gmail.com>
2023-08-29 15:53:30 -0700

    singly linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *current = *head, *del;
 
-	if (!head)
+	if (!*head)
 		return (-1);
 	if (index == 0)
 	{
