395999eeef17e75e577c032b8002b69d4c76f38f
armolas <armolas06@gmail.com>
2023-08-28 17:21:58 -0700

    more singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 		i++;
 	}
-	if (!current)
+	if (!current->next)
 		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (!new)
