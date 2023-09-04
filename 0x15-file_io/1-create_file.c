9b0f363be7e87613df5b9bc48420931f9c0c3191
armolas <armolas06@gmail.com>
2023-09-04 07:38:16 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * create_file - creates a new file
+ * @filename:the nale of the file
+ * @text_content: content of the file
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, count, bw;
+
+	if(!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
+	if (fd < 0)
+		return (-1);
+	for (count = 0 ; text_content[count] ; count++)
+		;
+	bw = write(fd, text_content, count);
+	if (bw < 0)
+		return (-1);
+	return (1);
+}
