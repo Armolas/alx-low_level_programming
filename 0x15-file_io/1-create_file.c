5c075b47bb8050628ee91e6a3c11c97a48d6fdc9
armolas <armolas06@gmail.com>
2023-09-04 07:59:27 -0700

    ii

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,10 +15,10 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 	if (!text_content)
-		return (-1);
+		return (1);
 	for (count = 0 ; text_content[count] ; count++)
 		;
-	bw = write(fd, text_content, count);
+	bw = write(fd, text_content, count + 1);
 	if (bw < 0)
 		return (-1);
 	return (1);
