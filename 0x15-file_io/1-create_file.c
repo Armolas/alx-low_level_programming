1c03888a5774ca3e07df7ab4881aaa71be211047
armolas <armolas06@gmail.com>
2023-09-04 08:18:57 -0700

    append to file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,11 +15,18 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 	if (!text_content)
+	{
+		close(fd);
 		return (1);
+	}
 	for (count = 0 ; text_content[count] ; count++)
 		;
 	bw = write(fd, text_content, count);
 	if (bw < 0)
+	{
+		close(fd);
 		return (-1);
+	}
+	close(fd);
 	return (1);
 }
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * append_text_to_file - adds text to end of a text to a file
+ * @filename: the file name
+ * @text_content: text to be added
+ * Return: 1 or -1
+ */
+append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, bw, count;
+
+	if(!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd < 0)
+		return (-1);
+	if(!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+	for (count = 0 ; text_content[count] ; count++)
+		;
+	bw = write(fd, text_content, count);
+	if (bw < 0)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd)
+		return(1);
+}
