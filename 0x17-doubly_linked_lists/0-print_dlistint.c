fec4dc8847cb46ec0753985c012713033f175a18
Armolas <armolas06@gmail.com>
2023-10-12 22:45:09 +0000

    doubly linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all elements of a doubly linked list
+ * @h: head of the list
+ * Return: number of elements in the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	dlistint_t *current = h->next;
+	size_t nb  = 0;
+
+	if (!h)
+		return (nb);
+	printf("%d\n", h->n);
+	nb++;
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		nb++;
+	}
+	return (nb);
+}
