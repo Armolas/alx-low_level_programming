3062cc6575a3406b8be996f1c8757997ef9f68f0
Armolas <armolas06@gmail.com>
2023-10-13 02:27:09 +0000

    doubly linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,7 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = current->next;
 		i++;
 	}
-	if (i == idx)
+	if (current)
 	{
 		new->next = current;
 		new->prev = current->prev;
