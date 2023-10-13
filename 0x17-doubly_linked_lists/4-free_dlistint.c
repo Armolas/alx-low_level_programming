3123ca1af53fcd7453a5106456bf6a214033eff2
Armolas <armolas06@gmail.com>
2023-10-13 01:01:53 +0000

    doubly linked list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,11 +7,11 @@ void free_dlistint(dlistint_t *head)
 {
 	if (head)
 	{
-		while (head)
+		while (head->next)
 		{
 			head = head->next;
 			free(head->prev);
 		}
-		free(head->prev);
+		free(head);
 	}
 }
