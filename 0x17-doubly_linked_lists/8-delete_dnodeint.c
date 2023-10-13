e947a8c12b3db1960647121767b6556b70837aef
Armolas <armolas06@gmail.com>
2023-10-13 02:58:41 +0000

    doubly linked list

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -5,3 +5,39 @@
  * @index: index to delete
  * Return: 1 or -1
  */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *current = *head;
+
+	if (index == 0 && *head)
+	{
+		if ((*head)->next)
+		{
+			*head = (*head)->next;
+			(*head)->prev = NULL;
+		}
+		else
+			*head = NULL;
+		free(current);
+		return (1);
+	}
+	while (i < index && current)
+	{
+		current = current->next;
+		i++;
+	}
+	if (current)
+	{
+		if (current->next)
+		{
+			current->next->prev = current->prev;
+			current->prev->next = current->next;
+		}
+		else
+			current->prev->next = NULL;
+		free(current);
+		return (1);
+	}
+	return (-1);
+}
