179cc7124e38919f27c8b771e69382f048159206
Armolas <armolas06@gmail.com>
2023-10-16 22:51:57 +0000

    advanced

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - divides two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - finds the remainder of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
