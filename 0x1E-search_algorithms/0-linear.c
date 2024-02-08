532067a55735526c24dc75bf85fb801936eb7084
Armolas <armolas06@gmail.com>
2024-02-09 00:06:40 +0100

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,6 +9,8 @@
 int linear_search(int *array, size_t size, int value){
     size_t i;
 
+    if (array == NULL || size == 0)
+        return (-1);
     for (i = 0 ; i < size ; i++){
 
         if (array[i] == value){
