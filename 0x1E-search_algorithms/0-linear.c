e4857fb18b7ce8773fd075057ecf475caaae850e
Armolas <armolas06@gmail.com>
2024-02-09 00:13:06 +0100

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include "search_algos.h"
 /**
-*linear_search - searches for a value in an array of integers using the Linear search algorithm
-*@array: the array to search
-*@size: size of the array
-*@value: value to search for in the array
-*Return: the first index where value is located
+* linear_search - searches for a value in an array of integers using
+* the Linear search algorithm
+* @array: the array to search
+* @size: size of the array
+* @value: value to search for in the array
+* Return: the first index where value is located
 */
 int linear_search(int *array, size_t size, int value){
     size_t i;
