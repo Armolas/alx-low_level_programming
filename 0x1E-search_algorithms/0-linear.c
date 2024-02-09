d9d5a05e3a9b195a8fbcc2b29ecfb14def8dc3b3
Armolas <armolas06@gmail.com>
2024-02-09 18:03:52 +0100

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,11 +15,9 @@ int linear_search(int *array, size_t size, int value){
         return (-1);
     for (i = 0 ; i < size ; i++){
 
+        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
         if (array[i] == value){
             return (i);
-        }
-        else{
-            printf("Value checked array[%ld] = [%d]\n", i, array[i]);
         }
     }
     return (-1);
