0d7cec1f6f33e74b2b595f4fd372b78d645c3667
Armolas <armolas06@gmail.com>
2024-02-10 14:22:13 +0100

    1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -8,32 +8,28 @@
  * @value: the value to search for
  * Return: index of the value
 */
-int binary_search(int *array, size_t size, int value){
-    int low, high, m, i;
+int binary_search(int *array, size_t size, int value)
+{
+	int low, high, m, i;
 
-    low = 0;
-    high = size - 1;
-    m = size;
-    if (array == NULL || size == 0)
-        return (-1);
-    while (low <= high){
-        m = (low + high) / 2;
-        printf("Searching in array: ");
-        for (i = low ; i < high ; i++){
-            printf("%d, ", array[i]);
-        }
-        printf("%d\n", array[i]);
-        if (array[m] < value){
-            low = m + 1;
-        }
-        else if (array[m] > value)
-        {
-            high = m - 1;
-        }
-        else{
-            return (m);
-        }
-        
-    }
-    return (-1);
+	low = 0;
+	high = size - 1;
+	m = size;
+	if (array == NULL || size == 0)
+		return (-1);
+	while (low <= high)
+	{
+		m = (low + high) / 2;
+		printf("Searching in array: ");
+		for (i = low ; i < high ; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+		if (array[m] < value)
+			low = m + 1;
+		else if (array[m] > value)
+			high = m - 1;
+		else
+			return (m);
+	}
+	return (-1);
 }
