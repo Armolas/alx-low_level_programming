c3e59f76d050fadf1aad41206beaf3722c33e770
Armolas <armolas06@gmail.com>
2024-02-10 14:17:49 +0100

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,16 +8,17 @@
 * @value: value to search for in the array
 * Return: the first index where value is located
 */
-int linear_search(int *array, size_t size, int value){
+int linear_search(int *array, size_t size, int value)
+{
 	size_t i;
 
 	if (array == NULL || size == 0)
 		return (-1);
-	for (i = 0 ; i < size ; i++){
+	for (i = 0 ; i < size ; i++)
+	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value){
+		if (array[i] == value)
 			return (i);
-		}
 	}
 	return (-1);
 }
